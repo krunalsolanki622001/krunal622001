ca8f05fedbd3609cb0c532fb08c7edeb7aa451cb
krunalsolanki622001 <68775869+krunalsolanki622001@users.noreply.github.com>
2020-09-14 12:27:36 +0530

    Update reg.js

--- javascript/reg.js
+++ javascript/reg.js
@@ -1,69 +1,80 @@
- function registration()
-	{
+function myFunction() {
+  var x = document.getElementById("psw");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
-		var name= document.getElementById("t1").value;
-		var email= document.getElementById("t2").value;
-		var uname= document.getElementById("t3").value;
-		var pwd= document.getElementById("t4").value;			
-		var cpwd= document.getElementById("t5").value;
-		
-        //email id expression code
-		var pwd_expression = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
-		var letters = /^[A-Za-z]+$/;
-		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
-		if(name=='')
-		{
-			alert('Please enter your name');
-		}
-		else if(!letters.test(name))
-		{
-			alert('Name field required only alphabet characters');
-		}
-		else if(email=='')
-		{
-			alert('Please enter your user email id');
-		}
-		else if (!filter.test(email))
-		{
-			alert('Invalid email');
-		}
-		else if(uname=='')
-		{
-			alert('Please enter the user name.');
-		}
-		else if(!letters.test(uname))
-		{
-			alert('User name field required only alphabet characters');
-		}
-		else if(pwd=='')
-		{
-			alert('Please enter Password');
-		}
-		else if(cpwd=='')
-		{
-			alert('Enter Confirm Password');
-		}
-		else if(!pwd_expression.test(pwd))
-		{
-			alert ('Upper case, Lower case, Special character and Numeric letter are required in Password filed');
-		}
-		else if(pwd != cpwd)
-		{
-			alert ('Password not Matched');
-		}
-		else if(document.getElementById("t5").value.length < 6)
-		{
-			alert ('Password minimum length is 6');
-		}
-		else if(document.getElementById("t5").value.length > 12)
-		{
-			alert ('Password max length is 12');
-		}
-		else
-		{				                            
-               alert('Thank You for Login & You are Redirecting to Campuslife Website');
-			   // Redirecting to other page or webste code. 
-			   window.location = "http://www.campuslife.co.in"; 
-		}
-	}
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+   function validate()
+  {
+    var ps1 = document.getElementsByName("psw");
+    var ps2 = document.getElementsByName("psw-repeat");
+    if(ps1[0].value != ps2[0].value)
+    {
+      alert("Please Enter The Same password");
+    }
+    else
+    {
+      alert("Registration Successfull");
+    }  
+}
